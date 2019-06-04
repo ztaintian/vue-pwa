7f65c802671d42b4675e1f37a09d589b9aa8a44a
unknown <zhutiantian@ciicsh.local>
2019-06-04 15:51:31 +0800

    'h'

---
+++ pwa/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img @click="adddecimalCount" alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld  msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch, Emit } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// https://www.cnblogs.com/zyfd/p/9907925.html
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {
+  private  decimal: number = 6;
+  private  myThy(): void {
+    console.log(this.decimal);
+  }
+  @Watch('decimal')
+  private decimalCount(newValue: string, oldValue: string) {
+    console.log(newValue, oldValue);
+  }
+  @Emit()
+  private adddecimalCount(): void {
+    this.decimal = 3;
+  }
+}
+</script>
